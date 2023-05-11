0cb77198b223f7249e8f273fa39c423d676fbd50
MUKESH KUMAR <104102943+mukeshkumargeu@users.noreply.github.com>
2023-05-11 23:25:30 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,6 @@
-/*==================== MENU SHOW Y HIDDEN ====================*/
-
-
-/*===== MENU SHOW =====*/
-/* Validate if constant exists */
-
-
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
-
-
-/*==================== REMOVE MENU MOBILE ====================*/
-
-
-/*==================== ACCORDION SKILLS ====================*/
-
-
-/*==================== QUALIFICATION TABS ====================*/
-
-
-/*==================== SERVICES MODAL ====================*/
-
-
-/*==================== PORTFOLIO SWIPER  ====================*/
-
-
-/*==================== TESTIMONIAL ====================*/
-
-
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-
-
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
-
-
-/*==================== SHOW SCROLL UP ====================*/ 
-
-
-/*==================== DARK LIGHT THEME ====================*/ 
+var typed= new typed(".text",)
+String:["Frontend Developer", "YouTuber", "Ethical Hacker", "Web Developer",]
+typeSpeed:100,
+backSpeed:100,
+backDelay:1000,
+loop:true
